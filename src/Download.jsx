69501d672f1f930f11062ab7d82cafef88f34b04
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-04-02 08:39:30 +0900

    feat: Download 버튼 기능 구현

--- src/Download.jsx
+++ src/Download.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { saveAs } from 'file-saver';
+
 export default function Download() {
+  // 임시적인 데이터
+  const blob = new Blob(['dataset'], { type: 'text/plain' });
+
+  function handleClick() {
+    saveAs(blob);
+  }
+
   return (
     <div>
-      <div>훈련 데이터셋</div>
-      <div>테스트 데이터셋</div>
+      <span>다운로드</span>
+      <button type="button" onClick={handleClick}>
+        DOWNLOAD
+      </button>
     </div>
   );
 }
--- src/Download.test.jsx
+++ src/Download.test.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+import { saveAs } from 'file-saver';
+
 import Download from './Download';
 
+jest.mock('file-saver');
+
 describe('Download', () => {
-  global.URL.createObjectURL = jest.fn();
+  saveAs.mockImplementation((item) => item);
 
-  it('"다운로드" 버튼을 클릭하면 파일을 다운로드 할 수 있다', () => {
-    const downloadData = jest.fn();
-
+  it('"DOWNLOAD" 버튼을 클릭하면 파일을 다운로드 할 수 있다', () => {
     const { queryByText } = render((
       <Download />
     ));
 
-    expect(queryByText('download')).not.toBeNull();
+    expect(queryByText('DOWNLOAD')).not.toBeNull();
 
-    fireEvent.click(queryByText('download'));
+    fireEvent.click(queryByText('DOWNLOAD'));
 
-    expect(downloadData()).toBeCalled();
+    expect(saveAs).toBeCalled();
   });
 });
