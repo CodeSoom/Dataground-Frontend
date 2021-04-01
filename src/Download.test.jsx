e8b1621a28b4ce92e6419249490c9e54a7194f44
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-04-02 03:51:27 +0900

    test: Download.test.jsx 테스트 추가

--- src/Download.test.jsx
+++ src/Download.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Download from './Download';
@@ -19,5 +19,19 @@ describe('Download', () => {
     ));
 
     expect(container).toHaveTextContent('테스트 데이터셋');
+  });
+
+  it('"training set"을 클릭하면 훈련 데이터셋을 내려받을 수 있다', () => {
+    const downloadTrainingSet = jest.fn();
+
+    const { container } = render((
+      <Download />
+    ));
+
+    expect(container.querySelector('a')).not.toBeNull();
+
+    fireEvent(container.querySelector('a'));
+
+    expect(downloadTrainingSet()).toBeCalled();
   });
 });
