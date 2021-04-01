d5ce9cfca8c7c9769c76204ee510160fd208eb2d
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-04-02 00:27:06 +0900

    add: Download.jsx 추가 및 테스트케이스 통과

---
+++ src/Download.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Download() {
+  return (
+    <div>
+      <div>훈련 데이터셋</div>
+      <div>테스트 데이터셋</div>
+    </div>
+  );
+}
--- src/Download.test.jsx
+++ src/Download.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import Download from './Download';
 
@@ -9,8 +10,7 @@ describe('Download', () => {
     const { container } = render((
       <Download />
     ));
-
-    expect(container.innerText).toHaveTextContent('훈련 데이터셋');
+    expect(container).toHaveTextContent('훈련 데이터셋');
   });
 
   it('"테스트 데이터셋"을 볼 수 있다.', () => {
@@ -18,6 +18,6 @@ describe('Download', () => {
       <Download />
     ));
 
-    expect(container.innerText).toHaveTextContent('테스트 데이터셋');
+    expect(container).toHaveTextContent('테스트 데이터셋');
   });
 });
