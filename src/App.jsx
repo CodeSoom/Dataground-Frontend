47c574028d0afcdd79400ff3547fe38e3e852825
Lee Sang Hoon <54464320+SangHoonly@users.noreply.github.com>
2021-04-08 00:26:24 +0900

    Revert "채점 정보 화면 표시 기능 추가"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 import Download from './Download';
 import Submit from './Submit';
-import Rating from './Rating';
 
 // 서버 주소에 따라 변경 예정
 const uploadFileUrl = 'http://localhost:8000/api/uploadFile';
@@ -12,8 +11,7 @@ const uploadFileUrl = 'http://localhost:8000/api/uploadFile';
 export default function App() {
   const [state, setState] = useState({
     uploadFile: {},
-
-  const { uploadFile, rating } = state;
+  });
 
   const { uploadFile } = state;
 
@@ -29,7 +27,6 @@ export default function App() {
     await axios({
       url: uploadFileUrl,
       method: 'post',
-      data: formData,
       data: uploadFile,
     });
   }
@@ -41,7 +38,6 @@ export default function App() {
         onChange={handleSubmitChange}
         onClick={handleSubmitClick}
       />
-      <Rating rating={rating} />
     </div>
   );
 }
